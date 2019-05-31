eb0ba3cf8a019c8beae6a9b8e3bf37215b057efa
Brandon Redshirt <brandon.redshirt@gmail.com>
2019-05-30 18:20:24 -0600

    Added single task removal, all tasks removal, and filter tasks functionality

--- app.js
+++ app.js
@@ -12,10 +12,17 @@ loadEventListeners();
 function loadEventListeners() {
     //Add task event
     form.addEventListener('submit', addTask);
+    //Remove task event
+    taskList.addEventListener('click', removeTask);
+    //Clear all tasks event
+    clearBtn.addEventListener('click', clearTasks);
+    //Filter tasks event
+    filter.addEventListener('keyup', filterTasks);
 }
 
 //Add task function
 function addTask(e) {
+    //Verify if there is a task to add
     if (taskInput.value === '') {
         alert('Add a task');
     }
@@ -41,4 +48,38 @@ function addTask(e) {
     taskInput.value = '';
 
     e.preventDefault();
+}
+
+//Remove Task function
+function removeTask(e) {
+    //Check if the target to remove is the correct target
+    if (e.target.parentElement.classList.contains('delete-item')) {
+        if (confirm('Are you sure?')) {
+            //Select the targeted li by bubbling up twice out of the i element
+            e.target.parentElement.parentElement.remove();
+        }
+    }
+}
+
+//Clear Tasks function
+function clearTasks() {
+    //Loop through and remove each child
+    while (taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+    }
+}
+
+//Filter tasks function
+function filterTasks(e) {
+    //Set the target to lowercase and grab the text from the input
+    const text = e.target.value.toLowerCase();
+
+    document.querySelectorAll('.collection-item').forEach(function(task) {
+        const item = task.firstChild.textContent;
+        if (item.toLowerCase().indexOf(text) != -1) {
+            task.style.display = 'block';
+        } else {
+            task.style.display = 'none';
+        }
+    });
 }
